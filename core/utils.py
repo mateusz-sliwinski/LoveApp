66e951e791453973661d6e7a7495dcee3d66a1e3
Mati1432 <mateuszsliwinski9@gmail.com>
2021-10-28 17:13:32 +0200

    add randomize

--- core/utils.py
+++ core/utils.py
@@ -1,1 +1,12 @@
 """Utils.py files."""
+from random import randint
+
+
+def randomize(all_users_count, actually_user):
+
+    x = randint(1, all_users_count)
+    if x == actually_user:
+        x = randint(1, all_users_count)
+        return x
+    return x
+
--- core/views.py
+++ core/views.py
@@ -3,14 +3,22 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.views.generic import TemplateView
 
+from accounts.models import CustomUser
+from core.utils import randomize
+
 
 class RandomPartner(TemplateView):
     template_name = 'random_partner.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        current_user = self.request.user.id
+        all_photo = CustomUser.objects.all().count()
 
         if 'Dislike' in self.request.GET:
+            randomize(all_photo, current_user)
+            print(all_photo)
+            print(current_user)
             print('jestem Dislike')
 
         if 'Like' in self.request.GET:
