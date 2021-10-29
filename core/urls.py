2c89d18a763056251d2203a519e3b02b63ed65c4
Mati1432 <mateuszsliwinski9@gmail.com>
2021-10-29 14:24:47 +0200

    fix ajax

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,12 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from .views import RandomPartner
+from .views import RandomPartner, RandomPartnerList
 
 app_name = 'core'
 
 urlpatterns = [
     path('random/', login_required(RandomPartner.as_view()), name='random'),
+    path('ajax/random/', login_required(RandomPartnerList.as_view()), name='ajax_random_person'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,16 @@
 """Views.py files."""
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 from accounts.models import CustomUser, PhotoUser
 from core.utils import randomize
 
 
-class RandomPartner(TemplateView):
-    template_name = 'random_partner.html'
+class RandomPartnerList(TemplateView):
+    template_name = 'random_person_list.html'
+    success_url = reverse_lazy('core:random')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -17,7 +19,7 @@ class RandomPartner(TemplateView):
 
         if 'Dislike' in self.request.GET:
             random = randomize(all_photo, current_user_id)
-            context['like'] = PhotoUser.objects.get(custom_user=random)
+            context['photo'] = PhotoUser.objects.filter(custom_user=random).all()
             return context
 
         if 'Like' in self.request.GET:
@@ -28,7 +30,26 @@ class RandomPartner(TemplateView):
     def get(self, request, *args, **kwargs):  # noqa D102
         html = render_to_string(
             self.template_name,
-            {'like': self.get_context_data()},
+            {'randomPerson': self.get_context_data()},
             request=self.request,
         )
+        print(self.get_context_data())
         return HttpResponse(html)
+
+
+class RandomPartner(TemplateView):
+    template_name = 'random_partner.html'
+
+    # test
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_user_id = self.request.user.id
+        all_photo = CustomUser.objects.all().count()
+        random = randomize(all_photo, current_user_id)
+        context['list'] = PhotoUser.objects.filter(custom_user=random).all()
+        return context
+
+    # def dispatch(self, request, *args, **kwargs):
+    #
+    #     print(self.get_context_data())
+    #     return super().dispatch(request, *args, **kwargs)
