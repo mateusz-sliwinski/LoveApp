9ddff7d8ec1f713c7f63aa077c0dc872dba25319
Mati1432 <mateuszsliwinski9@gmail.com>
2021-10-29 12:44:55 +0200

    change function

--- core/utils.py
+++ core/utils.py
@@ -4,9 +4,10 @@ from random import randint
 
 def randomize(all_users_count, actually_user):
 
-    x = randint(1, all_users_count)
-    if x == actually_user:
+    while True:
         x = randint(1, all_users_count)
-        return x
+        if x != actually_user:
+            break
+
     return x
 
--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.views.generic import TemplateView
 
-from accounts.models import CustomUser
+from accounts.models import CustomUser, PhotoUser
 from core.utils import randomize
 
 
@@ -12,23 +12,23 @@ class RandomPartner(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        current_user = self.request.user.id
+        current_user_id = self.request.user.id
         all_photo = CustomUser.objects.all().count()
 
         if 'Dislike' in self.request.GET:
-            randomize(all_photo, current_user)
-            print(all_photo)
-            print(current_user)
-            print('jestem Dislike')
+            random = randomize(all_photo, current_user_id)
+            context['like'] = PhotoUser.objects.get(custom_user=random)
+            return context
 
         if 'Like' in self.request.GET:
             print('jestem Like')
 
+        return context
 
     def get(self, request, *args, **kwargs):  # noqa D102
         html = render_to_string(
             self.template_name,
-            {'random_person': self.get_context_data()},
+            {'like': self.get_context_data()},
             request=self.request,
         )
         return HttpResponse(html)
