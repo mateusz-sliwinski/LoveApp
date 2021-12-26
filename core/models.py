f22b1175d404cb125bb3234ff65d4da918a39ae0
Mati1432 <mateuszsliwinski9@gmail.com>
2021-12-26 23:29:34 +0100

    Add model Dashboard

--- core/models.py
+++ core/models.py
@@ -8,8 +8,16 @@ from accounts.models import CustomUser
 
 
 class Likes(models.Model):  # noqa D101
-    user_one = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, related_name='user_one')
-    user_two = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, related_name='user_two')
+    user_one = models.ForeignKey(
+        CustomUser,
+        on_delete=models.DO_NOTHING,
+        related_name='user_one'
+    )
+    user_two = models.ForeignKey(
+        CustomUser,
+        on_delete=models.DO_NOTHING,
+        related_name='user_two'
+    )
     status = models.CharField(max_length=45)
 
     class Meta:  # noqa D106
@@ -21,17 +29,80 @@ class Likes(models.Model):  # noqa D101
 
 
 class Thread(models.Model):  # noqa D101
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='+')
-    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='+')
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+    receiver = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+
+    class Meta:  # noqa D106
+        verbose_name = 'Thread'
+        verbose_name_plural = 'Threads'
+
+    def __str__(self):  # noqa D105
+        return f'{self.user} {self.receiver}'
 
 
 class Message(models.Model):  # noqa D101
-    thread = models.ForeignKey('Thread', related_name='+', on_delete=models.CASCADE,
-                               blank=True, null=True,
-                               )
-    sender_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='+')
-    receiver_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='+')
+    thread = models.ForeignKey(
+        'Thread',
+        related_name='+',
+        on_delete=models.CASCADE,
+        blank=True, null=True,
+    )
+    sender_user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+    receiver_user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+    image = models.ImageField(
+        upload_to='chat',
+        blank=True,
+        null=True
+    )
     text_body = models.CharField(max_length=1000)
-    image = models.ImageField(upload_to='chat', blank=True, null=True)
     date = models.DateTimeField(default=timezone.now)
     is_read = models.BooleanField(default=False)
+
+    class Meta:  # noqa D106
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+
+    def __str__(self):  # noqa D105
+        return f'{self.sender_user} {self.receiver_user}'
+
+
+class Dashboard(models.Model):
+    count_like = models.IntegerField()
+    count_dislike = models.IntegerField()
+    count_message_send = models.IntegerField()
+    count_message_take = models.IntegerField()
+    create_date = models.DateField()
+
+    message = models.ForeignKey(
+        Message,
+        on_delete=models.DO_NOTHING,
+    )
+
+    likes = models.ForeignKey(
+        Likes,
+        on_delete=models.DO_NOTHING,
+    )
+
+    class Meta:  # noqa D106
+        verbose_name = 'Dashboard'
+        verbose_name_plural = 'Dashboards'
+
+    def __str__(self):  # noqa D105
+        return f'{self.create_date}-{self.count_like}-{self.count_dislike}-' \
+               f'{self.count_message_send}-{self.count_message_take}'
